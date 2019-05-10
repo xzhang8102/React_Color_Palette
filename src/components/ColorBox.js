2e3e0744164621930e888595a389b18edcffa97f
zxw <xzhang305@gmail.com>
2019-05-10 00:19:18 -0400

    refactor colorbox using react hooks

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,35 +1,28 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import ColorBoxOverlay from './ColorBoxOverlay';
 import '../styles/ColorBox.css';
 
-class ColorBox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { copied: false };
-    this.onCopied = this.onCopied.bind(this);
-  }
-  onCopied() {
-    this.setState({ copied: true }, () => {
-      setTimeout(() => this.setState({ copied: false }), 1500);
-    });
-  }
+const ColorBox = ({ name, background }) => {
+  const [copied, setCopied] = useState(false);
 
-  render() {
-    const { name, background } = this.props;
-    return (
-      <CopyToClipboard text={background} onCopy={this.onCopied}>
-        <div style={{ background }} className="ColorBox">
-          <ColorBoxOverlay copied={this.state.copied} color={background} />
-          <div className="box-content">
-            <span>{name}</span>
-          </div>
-          <button className="copy-button">COPY</button>
-          <span className="see-more">More</span>
+  const onCopied = () => {
+    setCopied(true);
+    setTimeout(() => setCopied(false), 1500);
+  };
+
+  return (
+    <CopyToClipboard text={background} onCopy={onCopied}>
+      <div style={{ background }} className="ColorBox">
+        <ColorBoxOverlay copied={copied} color={background} />
+        <div className="box-content">
+          <span>{name}</span>
         </div>
-      </CopyToClipboard>
-    );
-  }
-}
+        <button className="copy-button">COPY</button>
+        <span className="see-more">More</span>
+      </div>
+    </CopyToClipboard>
+  );
+};
 
 export default ColorBox;
