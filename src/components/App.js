5a96578ba58df2a2a65e69b7c8ebdcdf49595a13
zxw <xzhang305@gmail.com>
2019-05-17 14:39:03 -0400

    Add react router into the app

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import seedColor from '../seedColor';
 import { generatePalette } from '../colorVariation';
 import '../styles/App.css';
 
 class App extends React.Component {
+  findPalette(id) {
+    return seedColor.find(palette => {
+      return palette.id === id;
+    });
+  }
+
   render() {
     return (
-      <>
-        <Palette palette={generatePalette(seedColor[4])} />
-      </>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home Page</h1>} />
+          <Route
+            exact
+            path="/palette/:id"
+            render={routeProps => (
+              <Palette
+                palette={generatePalette(
+                  this.findPalette(routeProps.match.params.id)
+                )}
+              />
+            )}
+          />
+        </Switch>
+      </BrowserRouter>
     );
   }
 }
