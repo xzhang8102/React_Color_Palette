b676c407ff0077870ea61e618fb1a63af994c6b7
zxw <xzhang305@gmail.com>
2019-05-21 21:42:14 -0400

    text color based on chroma-luminance

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { Link } from 'react-router-dom';
+import chroma from 'chroma-js';
 import ColorBoxOverlay from './ColorBoxOverlay';
 import '../styles/ColorBox.css';
 
@@ -12,17 +13,24 @@ const ColorBox = ({ name, background, detailedUrl }) => {
     setTimeout(() => setCopied(false), 1500);
   };
 
+  const isDark = chroma(background).luminance() <= 0.066;
+  const isLight = chroma(background).luminance() >= 0.608;
+
   return (
     <CopyToClipboard text={background} onCopy={onCopied}>
       <div style={{ background }} className="ColorBox">
         <ColorBoxOverlay copied={copied} color={background} />
         <div className="box-content">
-          <span>{name}</span>
+          <span className={isDark ? 'light-text' : undefined}>{name}</span>
         </div>
-        <button className="copy-button">COPY</button>
+        <button className={`copy-button ${isLight ? 'dark-text' : undefined}`}>
+          COPY
+        </button>
         {detailedUrl && (
           <Link to={detailedUrl} onClick={e => e.stopPropagation()}>
-            <span className="see-more">More</span>
+            <span className={`see-more ${isLight ? 'dark-text' : undefined}`}>
+              More
+            </span>
           </Link>
         )}
       </div>
--- src/components/ColorBoxOverlay.js
+++ src/components/ColorBoxOverlay.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import chroma from 'chroma-js';
 import '../styles/ColorBoxOverlay.css';
 
 const ColorBoxOverLay = ({ copied, color }) => {
+  const isLight = chroma(color).luminance() >= 0.608;
+
   return (
     <>
-      <div style={{ background: color }} className={`copy-overlay ${copied && 'show'}`} />
+      <div
+        style={{ background: color }}
+        className={`copy-overlay ${copied && 'show'}`}
+      />
       <div className={`overlay-msg ${copied && 'show'}`}>
-        <h1>COPIED</h1>
-        <p>{color}</p>
+        <h1 className={isLight ? 'dark-text' : undefined}>COPIED</h1>
+        <p className={isLight ? 'dark-text' : undefined}>{color}</p>
       </div>
     </>
   );
